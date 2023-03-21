ab72015e67cee2cd1cec6ea554b7b24086995934
CreatorAndrey <simandor@yandex.ru>
2023-03-21 10:21:13 +0100

    Вход + переход на страницу поиска. Добавлены некоторые исключения

--- main_mcs.py
+++ main_mcs.py
@@ -11,8 +11,9 @@ from tkinter.messagebox import showerror, showinfo
 
 xpath_login = '//*[@id="login"]'
 xpath_password = '//*[@id="password"]'
-xpath_button = '/html/body/div[2]/div/section/div[2]/form/div[4]/input'
-url_entry = 'https://roboparts.ru/auth/'
+xpath_button = '/html/body/div/div/div/div/div/div/div/div[3]/form/button'
+xpath_notice = '//*[@id="block-system-main-menu"]/ul/li/ul/li[3]/ul/li[4]/a'
+url_entry = 'https://passport.cgu.mchs.ru/oauth/login?login_challenge=9043dbc4bed146c3ae16ef4e6c39fa7e'
 s = Service("chromedriver.exe")
 browser = Chrome(service=s)
 
@@ -22,26 +23,29 @@ def entry():
     except:
         showerror('Ошибка', 'Не удается перейти на страницу входа')
         return
+
     try:
         br_login = browser.find_element(By.XPATH, xpath_login)
         br_password = browser.find_element(By.XPATH, xpath_password)
         br_button = browser.find_element(By.XPATH, xpath_button)
     except:
-        showerror('Ошибка', 'Не найден элемент')
+        showerror('Ошибка','Не найден элемент')
         return
     login = txt_login.get()
     password = txt_password.get()
     br_login.send_keys(login)
     br_password.send_keys(password)
     br_button.click()
-    browser.refresh()
+    #browser.refresh()
     #sleep(2)
     br_login = browser.find_elements(By.XPATH, xpath_login)
     if len(br_login) == 0:
-        showinfo('Вход','Успешная авторизация')
+        showinfo('Вход','Успешный вход')
         btn_entry.configure(state='disabled')
+        br_notice = browser.find_element(By.XPATH, xpath_notice)
+        br_notice.click()
     else:
-        showerror('Ошибка', 'Ошибка авторизации')
+        showerror('Ошибка', 'Ошибка входа')
 
 
 window = Tk()
@@ -49,18 +53,14 @@ window.title('Программа')
 window.geometry('400x250')
 lbl_login = Label(window, text="Логин:")
 lbl_password = Label(window, text="Пароль:")
-txt_login = Entry(window, width=10)
-txt_password = Entry(window, width=10)
-btn_entry = Button(window, text='Авторизоваться', command=entry)
+lbl_demo = Label(window, text = 'ДЕМО', font=('Arial',18,'bold'))
+txt_login = Entry(window, width=20)
+txt_password = Entry(window, width=20)
+btn_entry = Button(window, text='Войти', width=17, command=entry)
 lbl_login.grid(column=0, row=0)
+lbl_demo.grid(column=3, row=0)
 txt_login.grid(column=1, row=0)
 lbl_password.grid(column=0, row=1)
 txt_password.grid(column=1, row=1)
-btn_entry.grid(column=0, row=2)
+btn_entry.grid(column=1, row=2)
 window.mainloop()
-
-
-
-
-
-
--- test.py
+++ test.py
@@ -9,16 +9,16 @@ from tkinter.messagebox import showerror,showinfo
 xpath_login = '//*[@id="login"]'
 xpath_password = '//*[@id="password"]'
 xpath_button = '/html/body/div[2]/div/section/div[2]/form/div[4]/input'
+xpath_other = '/html/body/div[2]/div/section/div[2]/form/noindex/div[1]/a'
 url_entry = 'https://roboparts.ru/auth/'
-s = Service("D:\Загрузки\chromedriver_win32\chromedriver.exe")
+s = Service('chromedriver.exe')
 browser = Chrome(service=s)
 browser.get(url_entry)
-br_login = browser.find_element(By.XPATH, xpath_login)
-br_password = browser.find_element(By.XPATH, xpath_password)
-br_button = browser.find_element(By.XPATH, xpath_button)
-
-br_login.send_keys('simandor@yandex.ru')
-br_password.send_keys('12345678')
+# br_login = browser.find_element(By.XPATH, xpath_login)
+# br_password = browser.find_element(By.XPATH, xpath_password)
+br_button = browser.find_element(By.XPATH, xpath_other)
+# br_login.send_keys('simandor@yandex.ru')
+# br_password.send_keys('12345678')
 br_button.click()
 browser.refresh()
 sleep(5)
