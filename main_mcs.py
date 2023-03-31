67100f61449a20f0d26c4b21755555332090c12c
CreatorAndrey <simandor@yandex.ru>
2023-03-31 13:05:03 +0200

    Версия 2.1, доделываю парсинг

--- main_mcs.py
+++ main_mcs.py
@@ -1,6 +1,8 @@
 # https://istories.media/workshops/2021/09/20/parsing-s-pomoshchyu-python-urok-2/
 # https://questu.ru/articles/81673/
 # https://stackoverflow.com/questions/29858752/error-message-chromedriver-executable-needs-to-be-available-in-the-path
+# https://www.geeksforgeeks.org/python-tkinter-scrolledtext-widget/
+import tkinter
 
 from selenium.webdriver import Chrome
 # from selenium.webdriver.common.keys import Keys
@@ -10,9 +12,12 @@ from selenium.webdriver.common.by import By
 from time import sleep
 from tkinter import *
 from tkinter.messagebox import showerror, showinfo
+from tkinter import ttk
+from tkinter import scrolledtext
 from openpyxl import load_workbook
 import logging
 from bs4 import BeautifulSoup
+import lxml
 
 logging.basicConfig(level=logging.DEBUG, filename='log.log', filemode='w',
                     format="%(asctime)s %(levelname)s %(message)s")
@@ -26,8 +31,11 @@ xpath_filter_open = '/html/body/div[2]/div/section/h1/span[2]'
 xpath_apply = '//*[@id="edit-submit-fgpn-license-notification-record"]'         # кнопка применить
 xpath_number = '//*[@id="edit-field-gl-registry-num-value"]'           # ссылка на поле с вводом номера регистрации
 xpath_open = '//*[@id="block-system-main"]/div/div[2]/div/table/tbody/tr/td[12]/div/a/span'     # забавный файлик
+xpath_open_a = '//*[@id="bootstrap-panel-3-body"]/div[2]/div[2]/div/a'
 class_date_registration = 'field-name-field-gl-registry-date'
 class_date_end = 'field-name-field-fgpn-notify-end-date'
+class_license = 'field-name-field-fs-subject'
+class_number_l = 'field-name-field-gl-number'
 
 url_entry = 'https://passport.cgu.mchs.ru/oauth/login?login_challenge=9043dbc4bed146c3ae16ef4e6c39fa7e'
 s = Service("chromedriver.exe")
@@ -158,7 +166,8 @@ def get_number(folder):
             logging.info('Click on the img_file')
             html = browser.page_source          # берем html страницы
             logging.info('Take the html')
-            list_excel = parser(html)            # передаем html в парсер и создаем список list_excel
+            #logging.info(html)
+            list_excel = parser(html, number)            # передаем html в парсер и создаем список list_excel
             logging.info('start insert data into excel')
 
             # начинаем подстановку в Excel
@@ -175,37 +184,261 @@ def get_number(folder):
     showinfo('Уведомление', 'Сбор информации завершен')
 
 
-def parser(html):
+def parser(html, number):
     logging.info('In parser function')
     list_excel = []
     try:
         soup = BeautifulSoup(html, 'html.parser')
+        soup2 = BeautifulSoup(html,'lxml')
         logging.info("The soup is creating successful")
     except:
         logging.exception("Problem with creating the soup")
         return
 
-    # парсинг даты регистрации
+    # парсинг даты регистрации D
     pr_date_registration = soup.find_all('div', class_=class_date_registration)
     logging.info('parsing date of registration')
     if len(pr_date_registration) == 0:
-        showinfo('Уведомление', 'Не найдена дата регистрации')
+        text_log.insert(END, f"Номер {number}, не найдена дата регистрации (D)")
     span_reg = pr_date_registration[0]
     date_reg = span_reg.find_next('span').string
     list_excel.append(date_reg + 'авто')
-    logging.info('Append to the list the date of reg.')
+    logging.info(f'Append to the list the date of reg. {date_reg}')
 
-    # парсинг даты завершения работ
+    # парсинг даты завершения работ E
     pr_date_end = soup.find_all('div', class_=class_date_end)
     logging.info('parsing date of ending')
     if len(pr_date_end) == 0:
-        showinfo('Уведомление', 'Не найдена дата окончания работ')
+        text_log.insert(END, f"Номер {number}, не найдена дата окончания работ (E)")
     span_end = pr_date_registration[0]
     date_end = span_end.find_next('span').string
     list_excel.append(date_end + 'авто')
-    logging.info('append in list date of end')
-    return list_excel
+    logging.info(f'append in list date of end {date_end}')
 
+    # парсинг уведомления о завершении работ F
+    pr_notify_end = soup.find_all('div', class_='field-name-field-fgpn-notify-end-rel')
+    if len(pr_notify_end) == 0:
+        text_log.insert(END, f"Номер {number}, не найдено уведомление о завершении работ (F)")
+    a = pr_notify_end[0]
+    notify_end = a.find_next('a').string
+    list_excel.append(notify_end + 'авто')
+    list_excel.append(notify_end + 'авто')
+    logging.info(f'append in list the {notify_end}')
+
+    # парсинг лицензиат G
+    pr_license = soup.find_all('div', class_=class_license)
+    logging.info('parsing pr_license')
+    if len(pr_license) == 0:
+        text_log.insert(END, f"Номер {number}, не найден лицензиат (G)\n")
+    a_lic = pr_license[0]
+    licen = a_lic.find_next('a').string
+    list_excel.append(licen + 'авто')
+    logging.info(f'append the {licen}')
+
+    # парсинг номер лицензии H
+    pr_number_l = soup.find_all('div', class_=class_number_l)
+    logging.info('parsing pr_number_l')
+    if len(pr_number_l) == 0:
+        text_log.insert(END, f"Номер {number}, не найден номер лицензии (H)\n")
+    div_number_l = pr_number_l[0]
+    number_l = div_number_l.find_next('div', class_='field-item').string
+    list_excel.append(number_l + 'авто')
+    logging.info(f'append in list the {number_l}')
+
+    #парсинг места осуществления деятельности I
+    try:
+        br_open_a = browser.find_element(By.XPATH, xpath_open_a)
+        br_open_a.click()
+        logging.info('Переход по ссылке для парсинга I')
+        html2 = browser.page_source
+        logging.info('Берем html2')
+        logging.info(html2)
+        browser.back()
+    except:
+        showerror('Ошибка', 'Нет перехода по ссылке Лицензия')
+        logging.exception('Ошибка перехода по ссылке')
+
+    # парсинг адреса выполнения работ J
+    pr_address_work = soup2.find('div', class_='field-name-field-gl-adresses')
+    logging.info('parsing pr_address_work')
+    if len(pr_address_work) == 0:
+        text_log.insert(END, f"Номер {number}, не найдены адреса выполнения работ (J)\n")
+    div_list = pr_address_work.find_all('div', class_='field-item')
+    addresses_work = ""
+    for i in div_list:
+        addresses_work += i.text + 'авто' + '\n'
+    list_excel.append(addresses_work + 'авто')
+    logging.info(f'append in list the {addresses_work}')
+
+    # парсинг даты заявления K
+    pr_receive_date = soup.find_all('div', class_='field-name-field-gl-receive-date')
+    logging.info('parsing pr_receive_date')
+    if len(pr_receive_date) == 0:
+        text_log.insert(END, f"Номер {number}, не найдена дата заявления (K)\n")
+    span_receive_date = pr_receive_date[0]
+    receive_date = span_receive_date.find_next('span').string
+    list_excel.append(receive_date + 'авто')
+    logging.info(f'append in list the {receive_date}')
+
+    # парсинг даты договора L
+    pr_data_deal = soup.find_all('div', class_='field-name-field-fgpn-notify-contract--date')
+    logging.info('parsing pr_data_deal')
+    if len(pr_data_deal) == 0:
+        text_log.insert(END, f"Номер {number}, не найдена дата договора (L)\n")
+    div_data_deal = pr_data_deal[0]
+    data_deal = div_data_deal.find_next('div', class_='field-item even').string
+    list_excel.append(data_deal + 'авто')
+    logging.info(f'append in list the {data_deal}')
+
+    # парсинг номер договора M
+    pr_number_deal = soup.find_all('div', class_='field-name-field-fgpn-notify-contract--number')
+    logging.info('parsing pr_number_deal')
+    if len(pr_number_deal) == 0:
+        text_log.insert(END, f"Номер {number}, не найден номер договора (M)\n")
+    div_number_deal = pr_number_deal[0]
+    number_deal = div_number_deal.find_next('div', class_='field-item even').string
+    list_excel.append(number_deal + 'авто')
+    logging.info(f'append in list the {number_deal}')
+
+    # парсинг заказчика работ N
+    pr_customer = soup.find_all('div', class_='field-name-field-fgpn-notify-contract--customer')
+    logging.info('parsing pr_inn')
+    if len(pr_customer) == 0:
+        text_log.insert(END, f"Номер {number}, не найден заказчик работ (N)\n")
+    div_customer = pr_customer[0]
+    customer = div_customer.find_next('div', class_='field-item even').string
+    list_excel.append(customer + 'авто')
+    logging.info(f'append in the list the {customer}')
+
+    # парсинг инн O
+    pr_inn = soup.find_all('div', class_='field-name-field-fgpn-notify-contract--inn')
+    if len(pr_inn) == 0:
+        text_log.insert(END, f"Номер {number}, не найден инн (О)\n")
+    div = pr_inn[0]
+    inn = div.find_next('div', class_='field-item even').string
+    list_excel.append(inn + 'авто')
+    logging.info(f'append in the list the {inn}')
+
+    # парсинг объекта P
+    pr_object = soup.find_all('div', class_='field-name-field-fgpn-object-name')
+    if len(pr_object) == 0:
+        text_log.insert(END, f"Номер {number}, не найдено имя объекта (P)\n")
+    div = pr_object[0]
+    object_name = div.find_next('div', class_='field-item even').string
+    list_excel.append(object_name + 'авто')
+    logging.info(f'append in the list the {object_name}')
+
+    # парсинг вид работы Q
+    pr_kind_work = soup.find_all('div', class_='field-name-field-fgpn-notify-kind')
+    if len(pr_kind_work) == 0:
+        text_log.insert(END, f"Номер {number}, не найден вид работы (Q)\n")
+    div = pr_kind_work[0]
+    kind_work = div.find_next('div', class_='field-item even').string
+    list_excel.append(kind_work + 'авто')
+    logging.info(f'append in the list the {kind_work}')
+
+    # парсинг номер проекта R
+    pr_object_number = soup.find_all('div', class_='field-name-field-fgpn-notify-project--number')
+    if len(pr_object_number) == 0:
+        text_log.insert(END, f"Номер {number}, не найден номер проекта (R)\n")
+    div = pr_object_number[0]
+    object_number = div.find_next('div', class_='field-item even').string
+    list_excel.append(object_number + 'авто')
+    logging.info(f'append in the list the {object_number}')
+
+    # парсинг дата проекта S
+    pr_project_data = soup.find_all('div', class_='field-name-field-fgpn-notify-project--date')
+    if len(pr_project_data) == 0:
+        text_log.insert(END, f"Номер {number}, не найдена дата проекта (S)\n")
+    div = pr_project_data[0]
+    project_data = div.find_next('div', class_='field-item even').string
+    list_excel.append(project_data + 'авто')
+    logging.info(f'append in the list the {project_data}')
+
+    # парсинг фамилия проектировщика T
+    pr_author_surname = soup.find_all('div', class_='field-name-field-fgpn-notify-project-author--f')
+    if len(pr_author_surname) == 0:
+        text_log.insert(END, f"Номер {number}, не найдена фамилия проектировщика (T)\n")
+    div = pr_author_surname[0]
+    author_surname = div.find_next('div', class_='field-item even').string
+    list_excel.append(author_surname + 'авто')
+    logging.info(f'append in the list the {author_surname}')
+
+    # парсинг имени проектировщика U
+    pr_author_name = soup.find_all('div', class_='field-name-field-fgpn-notify-project-author--i')
+    if len(pr_author_name) == 0:
+        text_log.insert(END, f"Номер {number}, не найдено имя проектировщика (U)\n")
+    div = pr_author_name[0]
+    author_name = div.find_next('div', class_='field-item even').string
+    list_excel.append(author_name + 'авто')
+    logging.info(f'append in the list the {author_name}')
+
+    # парсинг отчества проектировщика V
+    pr_author_ot = soup.find_all('div', class_='field-name-field-fgpn-notify-project-author--o')
+    if len(pr_author_ot) == 0:
+        text_log.insert(END, f"Номер {number}, не найдено отчество проектировщика (V)\n")
+    div = pr_author_ot[0]
+    author_ot = div.find_next('div', class_='field-item even').string
+    list_excel.append(author_ot + 'авто')
+    logging.info(f'append in the list the {author_ot}')
+
+
+    # парсинг номер аттестата W
+    pr_number_diplom = soup.find_all('div', class_='field-name-field-fgpn-notify-project-author--cert-number')
+    if len(pr_number_diplom) == 0:
+        text_log.insert(END, f"Номер {number}, не найден номер аттестата (W)\n")
+    div = pr_number_diplom[0]
+    number_diplom = div.find_next('div', class_='field-item even').string
+    list_excel.append(number_diplom + 'авто')
+    logging.info(f'append in the list the {number_diplom}')
+
+    # парсинг даты аттестата X
+    pr_data_diplom = soup.find_all('div', class_='field-name-field-fgpn-notify-project-author--cert-date')
+    if len(pr_data_diplom) == 0:
+        text_log.insert(END, f"Номер {number}, не найдена дата аттестат (X)\n")
+    div = pr_data_diplom[0]
+    data_diplom = div.find_next('div', class_='field-item even').string
+    list_excel.append(data_diplom + 'авто')
+    logging.info(f'append in the list the {data_diplom}')
+
+
+    # парсинг ответственного фамилия Y
+    pr_gl_employee = soup.find_all('div', class_='field-name-field-gl-employee--f')
+    if len(pr_gl_employee) == 0:
+        text_log.insert(END, f"Номер {number}, не найдена фамилия ответственного (Y)\n")
+    div = pr_gl_employee[0]
+    gl_employee = div.find_next('div', class_='field-item even').string
+    list_excel.append(gl_employee + 'авто')
+    logging.info(f'append in the list the {gl_employee}')
+
+    # парсинг ответственного имя Z
+    pr_gl_employee_name = soup.find_all('div', class_='field-name-field-gl-employee--i')
+    if len(pr_gl_employee_name) == 0:
+        text_log.insert(END, f"Номер {number}, не найдено имя ответственного (Z)\n")
+    div = pr_gl_employee_name[0]
+    gl_employee_name = div.find_next('div', class_='field-item even').string
+    list_excel.append(gl_employee_name + 'авто')
+    logging.info(f'append in the list the {gl_employee_name}')
+
+    # парсинг ответственного отчество АА
+    pr_gl_employee_ot = soup.find_all('div', class_='field-name-field-gl-employee--o')
+    if len(pr_gl_employee_ot) == 0:
+        text_log.insert(END, f"Номер {number}, не найдено отчество ответственного (АА)\n")
+    div = pr_gl_employee_ot[0]
+    gl_employee_ot = div.find_next('div', class_='field-item even').string
+    list_excel.append(gl_employee_ot + 'авто')
+    logging.info(f'append in the list the {gl_employee_ot}')
+
+    # парсинг ответственного снилса АB
+    pr_gl_employee_snils = soup.find_all('div', class_='field-name-field-gl-employee--snils')
+    if len(pr_gl_employee_snils) == 0:
+        text_log.insert(END, f"Номер {number}, не найден СНИЛС ответственного (АВ)\n")
+    div = pr_gl_employee_snils[0]
+    gl_employee_snils = div.find_next('div', class_='field-item even').string
+    list_excel.append(gl_employee_snils + 'авто')
+    logging.info(f'append in the list the {gl_employee_snils}')
+
+    return list_excel
 
 def start():
     folder_xl = txt_folder_xl.get()
@@ -214,7 +447,7 @@ def start():
 
 window = Tk()
 window.title('Программа')
-window.geometry('400x250')
+window.geometry('570x280')
 lbl_login = Label(window, text="Логин:")
 lbl_password = Label(window, text="Пароль:")
 lbl_demo = Label(window, text='ДЕМО', font=('Arial', 18, 'bold'))
@@ -224,6 +457,12 @@ txt_password = Entry(window, width=20)
 txt_folder_xl = Entry(window, width=20, state='disabled')
 btn_entry = Button(window, text='Войти', width=17, command=entry)
 btn_start = Button(window, text='Начать заполнение', state='disabled', command=start)
+
+# scrollbar = ttk.Scrollbar(orient="vertical", hei)
+# scrollbar.grid(column=2, row=5)
+text_log = scrolledtext.ScrolledText(window, width=40, height=5, wrap=tkinter.WORD)
+# scrollbar.config(command=text_log.yview)
+
 lbl_login.grid(column=0, row=0)
 lbl_demo.grid(column=2, row=0)
 txt_login.grid(column=1, row=0)
@@ -233,4 +472,5 @@ btn_entry.grid(column=1, row=2)
 lbl_folder.grid(column=0, row=3)
 txt_folder_xl.grid(column=1, row=3)
 btn_start.grid(column=1, row=4)
+text_log.grid(column=1, row=5, pady = 10, padx = 10)
 window.mainloop()
--- test.py
+++ test.py
@@ -1,3 +1,30 @@
-from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import lxml
+
+with open('D:\Работа\Авито парсер локальный сайт\html lxml.txt', 'rb') as f:
+    soup = BeautifulSoup(f, 'lxml')
+    # print(soup.prettify())
+    # pr_address_work = soup.find('table', class_='tableheader-processed')
+    # print(pr_address_work)
+
+    data = []
+    table = soup.find('table', class_='tableheader-processed')
+    table_body = table.find('tbody')
+    rows = table_body.find_all('tr')
+    for row in rows:
+
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        data.append([ele for ele in cols if ele])
+    print(data)
+
+
+    # div_list = pr_address_work.find_all('div', class_='field-item')
+    # addresses = ""
+    # for i in div_list:
+    #     addresses += i.text + '\n'
+    # print(addresses)
+
+    # div = pr_address_work[0]
+    # address_work = div.find_next('div', class_='field-item even').string
+    # print(address_work)
